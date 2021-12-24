21414ce8970f48a49e49017d39d1a82f0bf654c0
kloimhardt <markus.agwin@gmx.net>
2021-12-24 14:53:10 +0100

    update test and little-clojure for Phel 0.5.0

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
-# use Phel\Phel;
+use Phel\Phel;
 
-# $projectRootDir = __DIR__ . '/./';
+$projectRootDir = __DIR__ . '/./';
 
-# require $projectRootDir . 'vendor/autoload.php';
+require $projectRootDir . 'vendor/autoload.php';
 
-# Phel::run($projectRootDir, 'app\\server');
+Phel::run($projectRootDir, 'app\\server');
 
-use Phel\Compiler\Analyzer\Environment\GlobalEnvironmentSingleton;
+# following lines are used to call compiled version
 
-require_once __DIR__ . "/vendor/autoload.php";
-GlobalEnvironmentSingleton::initialize(); // I try to ride of this line in a later release. 
-require_once __DIR__ . "/out/app/server.php";
+# use Phel\Compiler\Analyzer\Environment\GlobalEnvironmentSingleton;
+
+# require_once __DIR__ . "/vendor/autoload.php";
+# GlobalEnvironmentSingleton::initialize(); // I try to ride of this line in a later release.
+# require_once __DIR__ . "/out/app/server.php";
