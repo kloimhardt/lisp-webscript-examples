a108423b4a7c600aeedbcb606e7bc3e6a27aa545
kloimhardt <markus.agwin@gmx.net>
2021-12-24 20:03:26 +0100

    index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 
-use Phel\Phel;
+#use Phel\Phel;
 
-$projectRootDir = __DIR__ . '/./';
+#$projectRootDir = __DIR__ . '/./';
 
-require $projectRootDir . 'vendor/autoload.php';
+#require $projectRootDir . 'vendor/autoload.php';
 
-Phel::run($projectRootDir, 'app\\server');
+#Phel::run($projectRootDir, 'app\\server');
 
 # following lines are used to call compiled version
 
-# use Phel\Compiler\Analyzer\Environment\GlobalEnvironmentSingleton;
+ use Phel\Compiler\Analyzer\Environment\GlobalEnvironmentSingleton;
+// include 'vendor.phar';
+// require_once "autoload.php";
 
-# require_once __DIR__ . "/vendor/autoload.php";
-# GlobalEnvironmentSingleton::initialize(); // I try to ride of this line in a later release.
-# require_once __DIR__ . "/out/app/server.php";
+ require_once __DIR__ ."/vendor/autoload.php";
+ GlobalEnvironmentSingleton::initialize(); // I try to ride of this line in a later release.
+ require_once __DIR__ . "/out/app/server.php";
